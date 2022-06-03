c0d563cc7a2a26d50c12ecc8a61a053c39d3618a
NOONECARES2 <79277037+NOONECARES2@users.noreply.github.com>
2022-06-03 17:15:23 +0300

    noonecares2.github.io

---
+++ app.js
@@ -0,0 +1,13 @@
+//Функции
+//Function Declaration
+// function greet(nick){
+//     console.log('Привет - ', nick)
+// }
+//Function Expression
+// const greet2=function greet2(nick){
+// console.log('Привет- 2 ', nick)
+// }
+// greet('Лена')
+// greet2('Лена')
+// console.log(typeof greet)
+// console.dir(greet)
